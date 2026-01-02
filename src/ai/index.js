613739f714142b9eadda3ca1f08fa3b1c63a9ea4
Brandon Sandoval <officialbrandonsandoval@gmail.com>
2026-01-02 11:26:06 -0700

    fix: export name mismatches in AI module

--- src/ai/index.js
+++ src/ai/index.js
@@ -2,8 +2,8 @@
  * PONS AI Module Index
  */
 
-import { scoreLead, scoreLeads } from './leadScoring.js';
-import { prioritizeDeal, prioritizeDeals } from './dealPrioritization.js';
+import { scoreLead, scoreAllLeads as scoreLeads } from './leadScoring.js';
+import { prioritizeDeal, prioritizeAllDeals as prioritizeDeals } from './dealPrioritization.js';
 import { generateInsights, getNextBestAction } from './insightEngine.js';
 import { generateRecommendations, getTopAction, getIgnoreRisks } from './actionRecommendations.js';
 
